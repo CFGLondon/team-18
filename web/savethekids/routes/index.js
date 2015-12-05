f236366fea8a56bd8fbef4d71ea4b35202e71293
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2015-12-05 01:50:49 +0000

    added routes for auth

--- web/savethekids/routes/index.js
+++ web/savethekids/routes/index.js
@@ -14,4 +14,19 @@ router.get('/signup', function(req, res, next){
   res.render('lol', {title: 'Signup'});
 });
 
+router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+      successRedirect : '/profile',
+      failureRedirect : '/'
+  }));
+
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;

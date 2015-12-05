14a4ab6e5fba903d2d26a7dad4d05c6334976915
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2015-12-05 01:12:51 +0000

    new approach

--- web/savethekids/routes/index.js
+++ web/savethekids/routes/index.js
@@ -1,39 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/*
-Parse.FacebookUtils.init({ // this line replaces FB.init({
-  appId      : '1498622890439553', // Facebook App ID
-  status     : true,  // check Facebook Login status
-  cookie     : true,  // enable cookies to allow Parse to access the session
-  xfbml      : true,  // initialize Facebook social plugins on the page
-  version    : 'v2.5' // point to the latest Facebook Graph API version
-});
-
-*/
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('lol', { title: 'Express' });
 });
 
 router.get('/login', function(req, res, next){
-  Parse.FacebookUtils.logIn(null, {
-    success: function(user) {
-      if (!user.existed()) {
-        alert("User signed up and logged in through Facebook!");
-      } else {
-        alert("User logged in through Facebook!");
-      }
-    },
-    error: function(user, error) {
-      alert("User cancelled the Facebook login or did not fully authorize.");
-    }
-  });
+  res.render('lol', {title: 'login'});
 });
 
 router.get('/signup', function(req, res, next){
-  res.render('index', {title: 'Signup'});
+  res.render('lol', {title: 'Signup'});
 });
 
 module.exports = router;
